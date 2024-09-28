04192335cd1ca46443b9b0a0f607fb6ebcd1e1a7
pangGou816 <panggou816@gmail.com>
2024-09-28 14:22:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
    os.system( 'pip uninstall pytubefix moviepy' )
 
-   os.system( 'pip install pytubefix==6.2.2' )
+   os.system( 'pip install pytubefix' )
    os.system( 'pip install moviepy==1.0.3' )
 
    sys.exit()
