136f42fc17f40fc43b2b73257fc00a8aad37f2fc
pangGou816 <panggou816@gmail.com>
2025-03-28 16:10:37 +0800

    --allow-empty-message

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 if __name__ == '__main__':
    os.system( 'pip uninstall -y pytubefix moviepy' )
 
-   os.system( 'pip install pytubefix moviepy==1.0.3' )
+   os.system( 'pip install git+https://github.com/felipeucelli/pytubefix.git@ecef8fb' )
+   os.system( 'pip install moviepy==1.0.3' )
 
    sys.exit()
